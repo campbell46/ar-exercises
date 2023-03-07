28ef89cbb948a128cb0ecfd8c198ea42ea125ea8
Sarah Campbell <sarahcampbell4646@gmail.com>
2023-03-07 18:38:24 -0500

    Complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,9 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter your store name"
+@store_name = $stdin.gets.chomp
+
+Ottawa = Store.create(name: @store_name)
+
+Ottawa.errors.each { |error| puts error.full_message } 
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,20 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :apparel_store
+
+  # def has_apparel
+  #   mens_apparel == true || womens_apparel == true
+  # end
+
+  private
+
+  def apparel_store
+    unless mens_apparel == true || womens_apparel == true
+      errors.add :womens_apparel, message = "Must include minimum one type of apparel"
+      errors.add :mens_apparel, message = "Must include minimum one type of apparel"
+    end
+  end
+
 end
